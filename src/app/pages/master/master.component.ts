d8eccde3f50985719b2f31443295238b0399dcfe
parth-tagadiya <parthtagadiya9199@gmail.com>
2021-10-02 00:41:08 +0530

    Fixed size and scroll, added mouse pointer

--- src/app/pages/master/master.component.ts
+++ src/app/pages/master/master.component.ts
@@ -9,11 +9,14 @@ import io from 'socket.io-client';
 export class MasterComponent implements OnInit {
 
   title = 'co-browsing-front';
+  roomId = "test"
   socket: any;
+  userList: any[] = [];
+  username = "master";
 
   ngOnInit(): void {
-    this.socket = io("http://192.168.1.6:5000");
-    this.getFrameData();
+    this.socket = io("http://192.168.1.5:5000");
+
     let element = document.querySelector("html");
     let changes = new MutationObserver((mutations: MutationRecord[]) => {
       // mutations.forEach((mutation: MutationRecord) => {
@@ -31,35 +34,96 @@ export class MasterComponent implements OnInit {
       // characterDataOldValue: true
     });
 
+    this.loadData();
+  }
+
+  loadData() {
     this.connectSocket();
+    // this.sendFrameData();
+    this.newUserConnected();
+    this.mouseChange();
+    this.disconnectUser();
   }
 
   @HostListener('window:resize', ['$event'])
   onResize(event: any) {
-    console.log(window.innerWidth, window.outerWidth, window.innerHeight, window.outerHeight);
-    this.socket.emit("sendSize", { innerWidth: window.innerWidth, innerHeight: window.innerHeight, room: "test" })
+    this.sendSize();
   }
 
   @HostListener('window:scroll', ['$event'])
   scrollHandler(event: any) {
-    console.log("Scroll Event", document.documentElement.scrollTop);
-    this.socket.emit("sendScroll", { scroll: document.documentElement.scrollTop, room: "test" })
+    this.sendScroll()
   }
 
-  getFrameData() {
-    setInterval(() => {
-      //@ts-ignore
-      let data = document.getElementById("container")?.innerHTML
-      this.socket.emit('sendContent', { html: data, room: "test" });
-      this.socket.emit("sendSize", { innerWidth: window.innerWidth, innerHeight: window.innerHeight, room: "test" })
-    }, 1000);
-    //@ts-ignore
-    // document.getElementById("content").innerHTML = data;
+  @HostListener('document:mousemove', ['$event'])
+  onMouseMove(event: any) {
+    this.sendMouseMove(event)
   }
 
   connectSocket() {
-    this.socket.emit('join', { rooms: "test" });
-
+    this.socket.emit('join', { roomId: this.roomId, isMaster: true });
   };
+
+  newUserConnected() {
+    this.socket.on("userJoined", (data: any) => {
+      //@ts-ignore
+      let htmlData = document.getElementById("container")?.innerHTML
+      this.socket.emit('sendContent', { html: htmlData, roomId: this.roomId, userId: data.userId });
+      this.sendSize();
+      this.sendScroll();
+    });
+  }
+
+  // sendFrameData() {
+  //   //@ts-ignore
+  //   let htmlData = document.getElementById("container")?.innerHTML
+  //   this.socket.emit('sendContent', { html: htmlData, roomId: this.roomId });
+  //   this.socket.emit("sendSize", { innerWidth: window.innerWidth, innerHeight: window.innerHeight, roomId: "test" });
+  // }
+
+  sendSize() {
+    this.socket.emit("sendSize", { innerWidth: window.innerWidth, innerHeight: window.innerHeight, roomId: this.roomId });
+  }
+
+  sendScroll() {
+    this.socket.emit("sendScroll", { scroll: document.documentElement.scrollTop, roomId: this.roomId })
+  }
+
+  sendMouseMove(event: any) {
+    this.socket.emit("sendMouse", { userId: this.socket.id, username: this.username, screenX: event.screenX, screenY: event.screenY, clientX: event.clientX, clientY: event.clientY, roomId: this.roomId })
+  }
+
+  mouseChange() {
+    this.socket.on("mouseChange", (data: any) => {
+      this.addUserList(data);
+    })
+  }
+
+  addUserList(data: any) {
+    if (!data.userId) {
+      return;
+    }
+    let index = this.userList.findIndex((user: any) => user.userId == data.userId)
+    data.position = { top: data.clientY + "px", left: data.clientX + "px" }
+    if (index == -1) {
+      this.userList.push(data)
+    } else {
+      this.userList[index] = data;
+    }
+  }
+
+  disconnectUser() {
+    this.socket.on("userDisconnected", (data: any) => {
+      this.removeUserList(data);
+    })
+  }
+
+  removeUserList(data: any) {
+    let index = this.userList.findIndex((user: any) => user.userId == data.userId)
+    data.position = { top: data.clientY + "px", left: data.clientX + "px" }
+    if (index > -1) {
+      this.userList.splice(index, 1)
+    }
+  }
 
 }
--- src/app/pages/slave/slave.component.ts
+++ src/app/pages/slave/slave.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import io from "socket.io-client";
 
 @Component({
@@ -8,41 +8,110 @@ import io from "socket.io-client";
 })
 export class SlaveComponent implements OnInit {
   socket: any;
+  roomId = "test"
+  userList: any[] = [];
+  username: any = "";
   constructor() { }
 
   ngOnInit(): void {
-    this.socket = io("http://192.168.1.6:5000");
+    this.socket = io("http://192.168.1.5:5000");
+    this.loadData();
+
+    this.username = prompt('What is your name?', '');
+  }
+
+  @HostListener('document:mousemove', ['$event'])
+  onMouseMove(event: any) {
+    this.sendMouseMove(event)
+  }
+
+  loadData() {
     this.connectSocket()
     this.getData();
     this.scrollChange();
     this.sizeChange();
+    this.mouseChange();
+    this.disconnectUser();
   }
 
   connectSocket() {
-    this.socket.emit('join', { rooms: "test" });
+    this.socket.emit('join', { roomId: this.roomId, isMaster: false, username: this.username });
   };
 
   getData() {
     this.socket.on("getContent", (data: any) => {
-      //@ts-ignore
-      document.getElementById("container").innerHTML = data.html;
+      if (data.userId == this.socket.id) {
+        // @ts-ignore
+        let iframe = document.getElementById('container');
+        // @ts-ignore
+        let iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
+        // document.getElementById("container").innerHTML = data.html;
+        // //@ts-ignore
+        iframeDoc.body.innerHTML = data.html
+        const cssLink = document.createElement("link");
+        cssLink.href = "../../../styles.css";
+        cssLink.rel = "stylesheet";
+        cssLink.type = "text/css";
+        iframeDoc.head.appendChild(cssLink);
+      }
     })
   }
 
   scrollChange() {
     this.socket.on("scrollChange", (data: any) => {
       //@ts-ignore
-      document.documentElement.scroll({ top: data.scroll, behavior: 'smooth' })
+      let iframe = document.getElementById('container');
+      //@ts-ignore
+      iframe.contentWindow.scrollTo(0, data.scroll);
+      // document.documentElement.scroll({ top: data.scroll, behavior: 'smooth' })
     });
   }
 
   sizeChange() {
     this.socket.on("sizeChange", (data: any) => {
       //@ts-ignore
-      document.getElementById("container")?.style.width = data.innerWidth
+      document.getElementById("container")?.width = data.innerWidth
+      // document.getElementById("container")?.style.width = data.innerWidth + "px"
       //@ts-ignore
-      document.getElementById("container")?.style.height = data.innerHeight
+      document.getElementById("container")?.height = data.innerHeight
+      // document.getElementById("container")?.style.height = data.innerHeight + "px"
     });
   }
 
+  sendMouseMove(event: any) {
+    this.socket.emit("sendMouse", { userId: this.socket.id, username: this.username, screenX: event.screenX, screenY: event.screenY, clientX: event.clientX, clientY: event.clientY, roomId: this.roomId })
+  }
+
+  mouseChange() {
+    this.socket.on("mouseChange", (data: any) => {
+      this.addUserList(data);
+    })
+  }
+
+  disconnectUser() {
+    this.socket.on("userDisconnected", (data: any) => {
+      this.removeUserList(data);
+    })
+  }
+
+  removeUserList(data: any) {
+    let index = this.userList.findIndex((user: any) => user.userId == data.userId)
+    data.position = { top: data.clientY + "px", left: data.clientX + "px" }
+    if (index > -1) {
+      this.userList.splice(index, 1)
+    }
+  }
+
+  addUserList(data: any) {
+    if (!data.userId) {
+      return;
+    }
+    let index = this.userList.findIndex((user: any) => user.userId == data.userId);
+    data.position = { top: data.clientY + "px", left: data.clientX + "px" }
+    if (index == -1) {
+      this.userList.push(data)
+    } else {
+      this.userList[index] = data;
+    }
+  }
 }
